41a463783598924cd84bb3b7996adde2dd58f8d9
KTibow <KTibow@users.noreply.github.com>
2020-08-27 13:29:25 -0700

    Black

--- models.py
+++ models.py
@@ -1,12 +1,13 @@
 from manage import db, app
 
+
 class User(db.Model):
-    __tablename__ = 'users'
-    
+    __tablename__ = "users"
+
     id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.String(120), index=True, unique=True)
     token = db.Column(db.String(120), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    
+
     def __repr__(self):
         return f"User {self.email}"
