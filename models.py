5413f4061e35cd804ce4b61fbcab1736c274def2
Kendell R <KTibow@users.noreply.github.com>
2020-08-27 13:28:23 -0700

    Create models.py

---
+++ models.py
@@ -0,0 +1,12 @@
+from manage import db, app
+
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(120), index=True, unique=True)
+    token = db.Column(db.String(120), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    
+    def __repr__(self):
+        return f"User {self.email}"
