e2fd1abb472a281f3a6ea08ffb21e3300ff6c42c
Kendell R <KTibow@users.noreply.github.com>
2020-08-28 08:18:23 -0700

    Update app.py

--- app.py
+++ app.py
@@ -55,10 +55,10 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String(120), index=True, unique=True)
-    token = db.Column(db.String(500), index=True, unique=True)
-    refresh = db.Column(db.String(500), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
+    url = db.Column(db.String(80), index=True, unique=True)
+    token = db.Column(db.String(642), index=True, unique=True)
+    refresh = db.Column(db.String(642), index=True, unique=True)
+    email = db.Column(db.String(60), index=True, unique=True)
 
     def __repr__(self):
         return f"User {self.email} at {self.url}"
